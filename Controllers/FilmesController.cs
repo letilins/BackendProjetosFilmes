8e85a3658426fdc2079347b707a634b6f93b8370
Letícia Lins <leticialins.rj@gmail.com>
2023-10-09 11:16:30 -0300

    Projeto WebApi Filmes - v1 - ajustando

--- Controllers/FilmesController.cs
+++ Controllers/FilmesController.cs
@@ -2,14 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 
-[Route("api/filmes")]
+[Route("api/filmes")]// raiz de uma api
 [ApiController]
 public class FilmesController : ControllerBase {
     private static List<Filme> filmes = new List<Filme> {
         new Filme { Id = 1, Titulo = "Filme 1", Diretor = "Direitor 1", Atores = "Ator 1, Atriz 1", Ano = 2020 }
 };
 
-[HttpGet]
+[HttpGet("lista-da-leticia2")]// complemento
 public ActionResult<IEnumerable<Filme>> Get(){
     return Ok(filmes);
 }
